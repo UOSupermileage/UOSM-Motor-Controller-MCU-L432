f6acfbc5d4dd4887fca24fc7c7f0ebe045bee120
Louis-Philippe St-Arnaud <99094186+louis-p-sta@users.noreply.github.com>
2025-11-29 14:36:03 -0500

    Motor is configured in velocity mode with 25000 PID_TORQUE_FLUX_LIMIT, as was the case in comp 2025. There is an option to change to torque mode (MOTOR_CONFIG_MODE_RAMP_MODE_MOTION). Torque mode with 25000 PID_TORQUE_FLUX limit was tested during test day on March 22, 2026 and acceleration was ok but speed too low. PID_TORQUE_FLUX limit for torque mode was changed to 32000.

--- Core/Inc/MotorParameters.h
+++ Core/Inc/MotorParameters.h
@@ -47,7 +47,7 @@
  *
  **********************************************************************************/
 
-#define MOTOR_CONFIG_MODE_RAMP_MODE_MOTION 1
+#define MOTOR_CONFIG_MODE_RAMP_MODE_MOTION 2
 /*********************************************************************************
  *
  * 		Fixed Throttle Value in RPM (Use for testing only)
@@ -233,8 +233,7 @@
 #if MOTOR_CONFIG_MODE_RAMP_MODE_MOTION == 2
 	#define MOTOR_CONFIG_PID_TORQUE_FLUX_LIMITS (uint32_t) 25000
 #elif MOTOR_CONFIG_MODE_RAMP_MODE_MOTION == 1
-        #define MOTOR_CONFIG_PID_TORQUE_FLUX_LIMITS (uint32_t)25000
-        #define MOTOR_MAX_TORQUE (int32_t)25000
+        #define MOTOR_CONFIG_PID_TORQUE_FLUX_LIMITS (uint32_t)32000
 #else
 	#define MOTOR_CONFIG_PID_TORQUE_FLUX_LIMITS (uint32_t)32000
 #endif
